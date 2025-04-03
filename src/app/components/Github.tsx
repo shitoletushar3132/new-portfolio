11b3c9d1f034f86ecceff3ec774c4b991e2698f9
shitoletushar3132 <shitoletushar3132@gmail.com>
2025-04-03 15:57:35 +0530

    error

--- src/app/components/Github.tsx
+++ src/app/components/Github.tsx
@@ -23,7 +23,7 @@ const GitHubStats = () => {
     // Assuming the response contains a list of contributions and you want to count them
     const totalContributions = Object.entries(data1?.total || {}).reduce(
       // @ts-expect-error: Type inference issue with Object.entries and numeric values
-      (total, [/* ignored */, count]) => total + count,
+      (total, [, /* ignored */ count]) => total + count,
       0
     );
 
@@ -58,41 +58,8 @@ const GitHubStats = () => {
       </div>
 
       <div>
-        <GitHubCalendar username="shitoletushar3132" year={year} />
+        <GitHubCalendar username="shitoletushar3132" year={"last"} />
       </div>
-
-      <a
-        className="flex items-center flex-wrap gap-8 px-4 py-3 bg-gray-800/50 rounded-lg mt-5 cursor-pointer"
-        href="https://github.com/shitoletushar3132"
-        target="__blank"
-      >
-        <div className="flex items-center gap-2">
-          <div className="p-1 bg-emerald-900/30 rounded">
-            <svg
-              viewBox="0 0 16 16"
-              width="16"
-              height="16"
-              className="fill-emerald-500"
-            >
-              <path d="M10.86 7c-.45-1.72-2-3-3.86-3-1.86 0-3.41 1.28-3.86 3H0v2h3.14c.45 1.72 2 3 3.86 3 1.86 0 3.41-1.28 3.86-3H14V7h-3.14zM7 10.2c-1.22 0-2.2-.98-2.2-2.2 0-1.22.98-2.2 2.2-2.2 1.22 0 2.2.98 2.2 2.2 0 1.22-.98 2.2-2.2 2.2z"></path>
-            </svg>
-          </div>
-          <span className="font-semibold">{userStats.contributions}</span>
-          <span className="text-gray-400">contributions</span>
-        </div>
-
-        <div className="flex items-center gap-2">
-          <Users size={16} className="text-gray-400" />
-          <span className="font-semibold">{userStats.followers}</span>
-          <span className="text-gray-400">followers</span>
-        </div>
-
-        <div className="flex items-center gap-2">
-          <GitPullRequest size={16} className="text-gray-400" />
-          <span className="font-semibold">{userStats.pullRequests}</span>
-          <span className="text-gray-400">Pull Requests</span>
-        </div>
-      </a>
     </div>
   );
 };
